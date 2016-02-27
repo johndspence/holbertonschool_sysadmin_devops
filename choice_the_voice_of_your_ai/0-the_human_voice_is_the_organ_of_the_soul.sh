986c298e7a0c0787a08ac0e2a4bfeb25f67bf734
John Spence <44@p2-1.hbtn.io>
2016-02-27 15:27:58 -0800

    Completed 0-the_human_voice_is_the_organ_of_the_soul.sh

--- choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+message=$1
+voice=$2
+server=$3
+filename=$(echo $message | awk '{print $1}')
+
+case $voice in
+
+"m")
+say -v Deranged -o "$filename".m4a $message
+;;
+"f")
+say -v Princess -o "$filename".m4a $message
+;;
+"x")
+say -v Zarvox -o "$filename".m4a $message
+;;
+	esac
+
+scp $filename.m4a admin@$server:/home/admin/Impossible_Octopus_Fitness
+
+echo "Listen to the message on $server/$filename.m4a"
